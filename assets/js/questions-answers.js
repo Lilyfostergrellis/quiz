2887ca60e532e2002d082ce944d01e29fa6d8c46
Lilyfostergrellis <lilyfostergrellis2@gmail.com>
2023-01-18 16:46:46 +0000

    questions/answers as an array of objects added

---
+++ assets/js/questions-answers.js
@@ -0,0 +1,56 @@
+// Questions (Array of objects) for the quiz
+
+const questions = [
+    {
+        title: 'What tag is used to define an image or add an image to an HTML page?',
+        answerChoices: ["<meta>", "<img>", "<table>", "<js>"],
+        correctAnswer: "<img>"
+    },
+    {
+        title: 'What tag can be used to insert a line break or blank line in an HTML document?',
+        answerChoices: ["<head></head>", "<title></title>", "<body></body>", "<br></br>"],
+        correctAnswer: "<br></br>"
+    },
+    {
+        title: 'How do you select elements with class name "example"?',
+        answerChoices: ["#example", "example", ".example", "*example"],
+        correctAnswer: ".example"
+    },
+   
+    {
+        title: 'Choose the correct HTML element for the largest heading:',
+        answerChoices: ["<heading>", "<h6>", "<head>", "<h1>"],
+        correctAnswer: "<h1>"
+    },
+    {
+        title: 'What does HTML stand for?',
+        answerChoices: ["Hyper Text Markup Language", "Hyperlinks and Text Markup Language", "Home Tool Markup Language", "High Texture Mark Link"],
+        correctAnswer: "Hyper Text Markup Language"
+    },
+    {
+        title: 'Which of these elements are all <table> elements?',
+        answerChoices: ["<thead> <body> <tr>", "<table> <tr> <td>", "<table> <head> <tfoot>", "<table> <tr> <tt>"],
+        correctAnswer: "<table> <tr> <td>"
+    },  
+    {
+        title: 'How do you select an element with id "lorem"?',
+        answerChoices: [".lorem", "#lorem", "*lorem", "lorem"],
+        correctAnswer: "#lorem"
+    },
+    {
+        title: 'How do you insert a comment in a CSS file?',
+        answerChoices: ["//this is a comment", "//this is a comment//", "'this is a comment", "/*this is a comment*/"],
+        correctAnswer: "/*this is a comment*/"
+    },
+  
+    {
+        title: 'How do you write "Hello World" in an alert box?',
+        answerChoices: ['msgBox("Hello World");', 'alert("Hello World");', 'msg("Hello World");', 'alertBox("Hello World");'],
+        correctAnswer: 'alert("Hello World");'
+    },
+    {
+        title: 'How do you create a function in JavaScript?',
+        answerChoices: ["function = exampleFunction()", "exampleFunction", "function exampleFunction()", "function:exampleFucntion()"],
+        correctAnswer: "function exampleFunction()"
+    }
+]
