eb6d8a9845c906e06b7c5b201febc4ef907f8095
Lilyfostergrellis <lilyfostergrellis2@gmail.com>
2023-01-19 16:28:35 +0000

    highscores js basic logic implemented

--- assets/js/highScores.js
+++ assets/js/highScores.js
@@ -1,1 +1,18 @@
 // js leaderboard
+let scoresContainer = document.querySelector("#highscores")
+let clearBtn = document.querySelector("#clear")
+
+showScores()
+
+function showScores(){
+}
+
+function getScores (){
+    let scores = localStorage.getItem("")
+    return JSON.parse(scores)
+}
+
+clearBtn.addEventListener("click", function(){
+    localStorage.clear();
+    scoresContainer.classList.add("hide");
+})
